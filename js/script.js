81822d2f3fe499331f61095b91aea6981f6aaddc
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-07-21 11:27:29 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function displayPage (list, page) {
    const endIndex = page * 9; /* store end index */
    const studentList = document.querySelector('.student-list'); /* UL element with class of "student-list" */ 
    studentList.innerHTML = ''; /* remove any previously displayed studnets */
-
+   /* loops over list parameter */
    for(let i = 0; i < list.length; i++) {
       if(i >= startIndex && i < endIndex ) {
          const student = list[i];
@@ -51,7 +51,43 @@ function displayPage (list, page) {
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
+/* list parameter - represent student data */
+function addPagination(list) {
+   /* variable to store number of pagination buttons needed */
+   const numOfButtons = Math.ceil(list.length / 9);
+   /* select UL element with class "link-list" */
+   const linkList = document.querySelector('ul.link-list');
+   /* remove any pagination buttons previously displayed */
+   linkList.innerHTML = '';
+   /* loop over variable for number of pages needed */
+   for(let i = 1; i < numOfButtons; i++) {
+      /* DOM elements needed to display pagination button */
+      let button = `
+         <li>
+            <button type="button">1</button>
+         </li>
+      `;
+      /* insert elements to link-list variable */
+      linkList.insertAdjacentHTML('beforeend', button);
+      /* select first pagination button */
+      const active = document.querySelector('button');
+      active.className = 'active';
+      /* create event listener */
+      linkList.addEventListener('click', (e) => {
+         if (e.target.tagName === 'BUTTON') {
+            /* remove active class from pagination button */
+            const remove = document.querySelector('.active');
+            remove.className = '';
+            /* add active class to pagination button that was just clicked */
+            e.target.className = 'active';
+            /* call showPage function passing list paramenter */
+            showPage(page, e.target.textContent);
+         }
+      });
+   };
+};
 
 
 // Call functions
+addPagination(data);
+showPage(data, 1);
