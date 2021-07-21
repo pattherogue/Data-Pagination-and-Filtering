707038a983326df73aa90f6185c5fb150e1b4f84
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-07-21 10:31:38 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,12 +17,34 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
+/* list parameter - represent student data */
+/* page parameter - represent page number */
 function displayPage (list, page) {
    const startIndex = (page * 9) - 9; /*store start index */
    const endIndex = page * 9; /* store end index */
    const studentList = document.querySelector('.student-list'); /* UL element with class of "student-list" */ 
-}
+   studentList.innerHTML = ''; /* remove any previously displayed studnets */
+
+   for(let i = 0; i < list.length; i++) {
+      if(i >= startIndex && i < endIndex ) {
+         const student = list[i];
+         let html = `
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src=${student.picture.large} alt="Profile Picture">
+               <h3>${student.name.first} ${student.name.last}</h3>
+               <span class="email">${student.email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">${student.registered.date}</span>
+            </div>
+         </li> 
+         `;
+         /* insert elements created to the "studentList" variable */
+         studentList.insertAdjacentHTML('beforeend', html);
+      }
+   }
+};
 
 
 /*
